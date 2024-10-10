24f739c5c049bc430f0192e10b70a68de9a99f1e
Lucius Morningstar <148591095+Exios66@users.noreply.github.com>
2024-10-09 21:31:40 -0500

    Create dateFormatter.js

---
+++ public/js/utils/dateFormatter.js
@@ -0,0 +1,5 @@
+export function formatDate(date) {
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+}
